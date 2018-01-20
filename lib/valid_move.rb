7bfcf71b7560f97e8b5d336959ac939931c7df0e
Rudia Kim <rudiakim@usc.edu>
2018-01-20 20:34:20 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -11,7 +11,7 @@ end
 
 def valid_move?(board, index)
 
-  if position_taken?(board, index) == " "
+  if position_taken?(board, index) == " " && position_taken?[board, index] == ""
         true
 
   else position_taken?(board, index) == nil
