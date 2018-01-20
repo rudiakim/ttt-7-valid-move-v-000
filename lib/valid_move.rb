911b4e2f7db6804e478593c2774ec837c7fbbd8d
Rudia Kim <rudiakim@usc.edu>
2018-01-20 19:37:22 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,2 @@
-# code your #valid_move? method here
-
-
-# re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+def valid_move?(position_taken?)
+end
